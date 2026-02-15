a30f314aa39f638cd3379583eaf637ea5600aee6
subramanyaraju1112 <subramanyaraju1112@gmail.com>
2026-02-16 00:54:51 +0530

    updated: logo addition

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
 import { RefreshCw } from "lucide-react"
 import { Button } from "./ui/button"
+import SportsMonkLogo from "../../public/assets/images/sportsmonk-logo.png"
+import Image from "next/image"
 
 const Header: React.FC = () => {
     return (
         <header className="max-w-7xl mx-auto p-4 flex justify-between items-center">
-            <h2 className="text-white text-2xl font-bold italic">Sportsmonk</h2>
+            <div className="flex gap-2 items-center">
+                <Image src={SportsMonkLogo} alt="sportsmonk-logo" width={32} height={32} />
+                <h2 className="text-white text-2xl font-bold italic">Sportsmonk</h2>
+            </div>
             <Button variant="outline"
                 className="flex items-center gap-2 bg-gray-700 text-gray-100 border-gray-600 hover:bg-gray-600">
                 <RefreshCw
